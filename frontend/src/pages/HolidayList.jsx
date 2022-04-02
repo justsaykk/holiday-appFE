2b912bc60bb819463ddff2ed66df3b1b626d1cf0
justsaykk <kaikein.woo@gmail.com>
2022-04-02 16:43:02 +0800

    changed urlcat

--- frontend/src/pages/HolidayList.jsx
+++ frontend/src/pages/HolidayList.jsx
@@ -1,19 +1,19 @@
 import {useEffect, useState} from "react"
 import urlcat from "urlcat"
 
-const BACKEND = process.env.REACT_APP_BACKEND;
+// const BACKEND = process.env.REACT_APP_BACKEND;
 
 function HolidayList() {
     const [holidays, setHolidays] = useState([]);
 
     useEffect(() => {
-        fetch(urlcat(BACKEND, "/api/holidays"))
+        fetch("https://holidayapp-backend.herokuapp.com/api/holidays")
         .then(res => res.json())
         .then((data) => {setHolidays(data)})
     }, []);
 
     const handleDelete = (id) => {
-        const url = urlcat(BACKEND, `/api/holidays.${id}`)
+        const url = urlcat(`https://holidayapp-backend.herokuapp.com/api/holidays/${id}`)
         fetch (url, {method: "DELETE"})
         .then((res) => res.json())
         .the((data) => setHolidays(data))
@@ -24,7 +24,7 @@ function HolidayList() {
             {holidays.map(el => {
                 <li key={el.id}>
                     <a href="">{el.name}</a>
-                    <span onClick={handleDelete(el._id)}>Delete</span>
+                    {/* <span onClick={handleDelete(el._id)}>Delete</span> */}
                 </li>
             })}
         </>
