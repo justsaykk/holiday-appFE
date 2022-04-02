ebf31ba334ccdc57527e299817893d71cd9ca819
justsaykk <kaikein.woo@gmail.com>
2022-04-02 12:01:33 +0800

    seeded

--- holiday-app/src/pages/Seed.js
+++ holiday-app/src/pages/Seed.js
@@ -1,4 +1,10 @@
 function Seed() {
+  fetch("https://holidayapp-backend.herokuapp.com/api/holidays/seed")
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+    });
+
   return (
     <>
       <h1>seed</h1>
