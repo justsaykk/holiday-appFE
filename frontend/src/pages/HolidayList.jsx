00022e17cdf529ab2d3888abd9c00d3e557bb1f0
justsaykk <kaikein.woo@gmail.com>
2022-04-02 16:03:59 +0800

    modified HolidayList

--- frontend/src/pages/HolidayList.jsx
+++ frontend/src/pages/HolidayList.jsx
@@ -1,11 +1,28 @@
+import {useEffect, useState} from "react"
+import urlcat from "urlcat"
+
+const BACKEND = process.env.REACT_APP_BACKEND;
+
 function HolidayList() {
+    const [holidays, setHolidays] = useState([]);
+
+    useEffect(() => {
+        fetch(urlcat(BACKEND, "/api/holidays"))
+        .then(res => res.json())
+        .then((data) => {setHolidays(data)})
+    }, []);
+
     return (
         <>
-            <ul>
-                <li>Hello World</li>
-            </ul>
+            {holidays.map(el => {
+                <li key={el.id}>
+                    <a href="">{el.name}</a>
+                    <a href="">Delete</a>
+                </li>
+            })}
         </>
     )
+
 }
 
-export default HolidayList
+export default HolidayList;
